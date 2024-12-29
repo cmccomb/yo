f10c45a0d3a50aef76b73b338896b060a2c73547
Chris McComb <ccmcc2012@gmail.com>
2024-12-29 16:43:15 -0500

    More of a testing framework

--- tests/run.zsh
+++ tests/run.zsh
@@ -1,72 +1,20 @@
 #!/usr/bin/env zsh
 
-function setup() {
-  # Make sure zsh exists, and install if not
-  if ! command -v zsh &> /dev/null; then
-    echo "Zsh is not installed. Installing..."
-    zsh <(curl -s https://cmccomb.com/yo/install)
-  fi
-  PASSES=0
-  FAILS=0
-}
-
-function cleanup() {
-  echo "Tests complete. ${PASSES} passed, ${FAILS} failed."
-  if [[ $FAILS -gt 0 ]]; then
-    exit 1
-  else
-    exit 0
-  fi
-}
-
-function answer_should_contain() {
-  # Parse arguments
-  local expected=$1
-  local query="${*:2}"
-
-  # Print test description
-  echo "Testing query: 'yo ${query}' (answer must contain \"${expected}\")"
-
-  # Measure start time
-  local start_time
-  start_time=$(gdate +%s.%N)
-
-  # Make variables
-  local output
-  output=$(eval "yo ${query} --quiet")
-
-  # Measure end time
-  local end_time
-  end_time=$(gdate +%s.%N)
-
-  # Calculate elapsed time
-  local elapsed_time
-  elapsed_time=$(echo "$end_time - $start_time" | bc | awk '{printf "%.2f", $0}')
-
-  if [[ $output == *"${expected}"* ]]; then
-    echo "  ✅ Test passed in ${elapsed_time}s with answer: ${output}"
-    ((PASSES+=1))
-    return 0
-  else
-    echo "  ❌ Test failed in ${elapsed_time}s with answer: ${output}"
-    ((FAILS+=1))
-    return 1
-  fi
-}
+source tests/utilities.zsh
 
 # Run setup
 setup
 
 # Run test for basic queries
-#answer_should_contain "Paris" "What is the capital of France"
+answer_should_contain "Paris" "What is the capital of France"
 
-## Run test for more queries with --surf flag
-#answer_should_contain "Rome" "--surf what city is the vatican located in --task-model"
-#
-## Test model overrides
+# Run test for more queries with --surf flag
+answer_should_contain "Rome" "--surf what city is the vatican located in --task-model"
+
+# Test model overrides
 answer_should_contain "Paris" "What is the capital of France --task-model"
-#answer_should_contain "Paris" "What is the capital of France --balanced-model"
-#answer_should_contain "Paris" "What is the capital of France --serious-model"
+answer_should_contain "Paris" "What is the capital of France --balanced-model"
+answer_should_contain "Paris" "What is the capital of France --serious-model"
 
 # Run cleanup
 cleanup
