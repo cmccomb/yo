7d4564f1176d86919340931712d43a5e01301a12
Chris McComb <ccmcc2012@gmail.com>
2024-12-29 19:32:15 -0500

    Decomposition test

---
+++ src/app.zsh
@@ -0,0 +1,99 @@
+#!/usr/bin/env zsh
+
+########################################################################################################################
+### THE YO #############################################################################################################
+########################################################################################################################
+
+### Define the Yo ######################################################################################################
+YO="✌️"
+readonly YO
+
+### All versioning follows semver as defined at https://semver.org/ ####################################################
+VERSION="0.1.0"
+readonly VERSION
+
+### Display version information ########################################################################################
+function show_version() {
+	echo "yo v${VERSION}"
+}
+
+### Display help instructions ##########################################################################################
+function show_help() {
+	cat <<-EOF
+		    yo - A command-line AI assistant.
+
+		    Usage:
+		      yo [options] [question]
+
+		    Description:
+		      If a question is provided, Yo will answer the question. Otherwise, Yo will enter an interactive session.
+
+		    Context Options:
+		      These options allow you to control the information that Yo uses to answer your question.
+
+		      First, we have static local options that introduce specific predefined knowledge:
+		        -u, --usage             Load this help message into Yo's context.
+
+		      Next, we have dynamic local options that pull in fress information from the local system:
+		        -c, --clipboard         Copy the contents of the clipboard into Yo's context.
+		        -d, --directory         Include a list of the files in the current directory in Yo's context.
+		        -f, --file "PATH"       Extract the specified file into Yo's context. Supports a variety of file formats,
+		                                including .pdf, .docx, .txt, .md, .py, .zsh. This flag can be repeated multiple times
+		                                to bring in multiple files.
+		        -y, --system            Run a few system commands and integrate the information into Yo's context.
+
+		      Finally, we have several option that require an internet connection:
+		        -s, --search "TERMS"    Perform a web search using the specified quoted terms and integrate the results into
+		                                Yo's context. This flag can be repeated multiple times to perform unique searches.
+		        -S, --surf              Perform a web search using LLM-chosen terms based on the question and integrate the
+		                                results into Yo's context.
+		        -w, --website "URL"     Extract the specified website into Yo's context. This flag can be repeated multiple
+		                                times to bring in multiple sites.
+
+		    Model Size Options:
+		      Yo uses four distinct models. In order of increasing size, they are: a task model (1B), a casual model (3B), a
+		      balanced model (7B), and a serious model (14B). By default, Yo uses the task model for summarization and other
+		      small tasks, the casual model for one-off questions, and the serious model for interactive sessions. You can
+		      override the default model by using the following options:
+		        -tm, --task-model       Use the task model
+		        -cm, --casual-model     Use the casual model
+		        -bm, --balanced-model   Use the balanced model
+		        -sm, --serious-model    Use the serious model
+
+		    General Purpose Options:
+		      These options provide general functionality.
+		        -h, --help              Show this help message and exit.
+		        -q, --quiet             Suppress all output except the answer.
+		        -v, --verbose           Enable verbose mode for detailed output.
+		        -V, --version           Show the version and exit.
+
+		    Examples:
+		      * Answer a question:
+		        $ yo "What is the capital of France?"
+
+		      * For simple cases, you can omit the quotes:
+		        $ yo what is the capital of france
+
+		      * Start an interactive session:
+		        $ yo
+
+		      * Integrate information from a file:
+		        $ yo --file src "How can I improve this source code?"
+
+		      * Integrate information from a URL:
+		        $ yo --website "https://en.wikipedia.org/wiki/Paris" how big is paris
+
+		      * Integrate Google search results:
+		        $ yo --search "what is the capital of Tobago"
+
+		      * Use LLM-selected search terms:
+		        $ yo --surf tell me about renewable energy trends.
+
+		      * Combine context sources:
+		        $ yo --file src --search "what is the capital of france"
+
+		      * Add verbosity to any of these commands
+		        $ yo what is the capital of france --verbose
+	EOF
+	return 0
+}
---
+++ src/settings.zsh
@@ -0,0 +1,85 @@
+#!/usr/bin/env zsh
+
+########################################################################################################################
+### CONSTANTS AND SETTINGS #############################################################################################
+########################################################################################################################
+
+# Common parameters
+GENERAL_USERNAME="bartowski"
+GENERAL_FINETUNING_STYLE="Instruct"
+GENERAL_MODEL_FILETYPE="gguf"
+
+# Estimating lengths
+TOKEN_ESTIMATION_CORRECTION_FACTOR=1.2
+CHARACTERS_PER_TOKEN=4
+
+# Generation lengths
+SEARCH_TERM_GENERATION_LENGTH=16
+ONEOFF_GENERATION_LENGTH=128
+INTERACTIVE_GENERATION_LENGTH=512
+COMPRESSION_GENERATION_LENGTH=128
+COMPRESSION_CHUNK_LENGTH=4096
+COMPRESSION_TRIGGER_LENGTH=1024
+
+# Context lengths
+SEARCH_TERM_CONTEXT_LENGTH=-1 # custom fit to prompt and generation length
+ONEOFF_CONTEXT_LENGTH=-1      # custom fit to prompt and generation length
+INTERACTIVE_CONTEXT_LENGTH=$((8 * INTERACTIVE_GENERATION_LENGTH))
+
+### Define parameters for the task model ###############################################################################
+
+# Model parameters
+TASK_USERNAME=${GENERAL_USERNAME}
+TASK_MODEL_SERIES="Llama-3.2"
+TASK_MODEL_FINETUNING_STYLE=${GENERAL_FINETUNING_STYLE}
+TASK_MODEL_SIZE="1B"
+TASK_MODEL_QUANT="Q4_K_M"
+TASK_MODEL_FILETYPE=${GENERAL_MODEL_FILETYPE}
+
+# Generation parameters
+TASK_MODEL_TEMP=0.2
+
+### Define parameters for the casual model #############################################################################
+
+# Model parameters
+CASUAL_GENERAL_USERNAME=${GENERAL_USERNAME}
+CASUAL_MODEL_SERIES="Qwen2.5"
+CASUAL_MODEL_FINETUNING_STYLE=${GENERAL_FINETUNING_STYLE}
+CASUAL_MODEL_SIZE="3B"
+CASUAL_MODEL_QUANT="Q4_K_M"
+CASUAL_MODEL_FILETYPE=${GENERAL_MODEL_FILETYPE}
+
+# Generation parameters
+CASUAL_MODEL_TEMP=0.2
+
+### Define parameters for the balanced model ###########################################################################
+BALANCED_GENERAL_USERNAME=${GENERAL_USERNAME}
+BALANCED_MODEL_SERIES="Qwen2.5"
+BALANCED_MODEL_FINETUNING_STYLE=${GENERAL_FINETUNING_STYLE}
+BALANCED_MODEL_SIZE="7B"
+BALANCED_MODEL_QUANT="Q4_K_M"
+BALANCED_MODEL_FILETYPE=${GENERAL_MODEL_FILETYPE}
+
+# Generation parameters
+BALANCED_MODEL_TEMP=0.2
+
+### Define the series model parameters #################################################################################
+SERIOUS_GENERAL_USERNAME=${GENERAL_USERNAME}
+SERIOUS_MODEL_SERIES="Qwen2.5-Coder"
+SERIOUS_GENERAL_FINETUNING_STYLE=${GENERAL_FINETUNING_STYLE}
+SERIOUS_MODEL_SIZE="14B"
+SERIOUS_MODEL_QUANT="IQ4_XS"
+SERIOUS_GENERAL_MODEL_FILETYPE=${GENERAL_MODEL_FILETYPE}
+
+# Generation parameters
+SERIOUS_MODEL_TEMP=0.2
+
+### Custom search API key ##############################################################################################
+GOOGLE_CSE_API_KEY="AIzaSyBBXNq-DX1ENgFAiGCzTawQtWmRMSbDljk"
+GOOGLE_CSE_ID="003333935467370160898:f2ntsnftsjy"
+GOOGLE_CSE_BASE_URL="https://customsearch.googleapis.com/customsearch/v1"
+readonly GOOGLE_CSE_API_KEY GOOGLE_CSE_ID GOOGLE_CSE_BASE_URL
+
+### Maximum length of file content to extract ##########################################################################
+MAX_FILE_CONTENT_LENGTH=300000
+
