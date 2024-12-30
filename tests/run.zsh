530abbd66542a81dd9dd4de90395bb411ef4758d
Chris McComb <ccmcc2012@gmail.com>
2024-12-29 20:16:14 -0500

    Looks like we can't go larger than the task model

--- tests/run.zsh
+++ tests/run.zsh
@@ -6,15 +6,10 @@ source tests/utilities.zsh
 setup
 
 # Run test for basic queries
-answer_should_contain "Paris" "What is the capital of France"
+answer_should_contain "Paris" "What is the capital of France --task-model"
 
 # Run test for more queries with --surf flag
-#answer_should_contain "Rome" "--surf what city is the vatican located in --task-model"
-
-# Test model overrides
-answer_should_contain "Paris" "What is the capital of France --task-model"
-#answer_should_contain "Paris" "What is the capital of France --balanced-model"
-#answer_should_contain "Paris" "What is the capital of France --serious-model"
+answer_should_contain "Rome" "--surf what city is the vatican located in --task-model"
 
 # Run cleanup
 cleanup
