4029543a7ae91874e42881f22a0e34cb08516706
Chris McComb <ccmcc2012@gmail.com>
2025-01-03 11:11:43 -0500

    Full coverage for static flags (#11)

--- tests/test_static_flags.zsh
+++ tests/test_static_flags.zsh
@@ -6,10 +6,8 @@ source tests/utilities.zsh
 # Run setup
 setup
 
-# Run test for basic queries
+# Test the --usage flag
 answer_should_contain "--help" "--task-model --usage what option can I use to see your help message"
-
-# Run test for basic queries
 answer_should_contain "--task-model" "--task-model --usage what are the options i can use to override the default model with the task model"
 
 # Run cleanup
