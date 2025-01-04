8b28ed2092ad3f1145a4a4429b3ac033a34fa13f
Chris McComb <ccmcc2012@gmail.com>
2025-01-04 10:28:39 -0500

    Tests now verify doubles

--- tests/test_online_flags.zsh
+++ tests/test_online_flags.zsh
@@ -11,9 +11,11 @@ answer_should_contain "-count" "--task-model --surf how do i output the total nu
 
 # Test the --search flag
 answer_should_contain "-count" "--task-model --search \"mdfind options\" how do i output the total number of matches from mdfind"
+answer_should_contain "-count" "--task-model --search \"mdfind options\" --search \"mdfind number of matches\" how do i output the total number of matches from mdfind"
 
 # Test the --website flag
 answer_should_contain "-count" "--task-model --website \"https://ss64.com/mac/mdfind.html\" how do i output the total number of matches from mdfind"
+answer_should_contain "-count" "--task-model --website \"https://ss64.com/mac/mdfind.html\" --website \"https://superuser.com/questions/1063269/the-mdfind-command-on-mac-os-x\" how do i output the total number of matches from mdfind"
 
 # Run cleanup
 cleanup
