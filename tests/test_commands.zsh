5a02f098963c34f37761ee2ff35f0726a273d7d3
Chris McComb <ccmcc2012@gmail.com>
2025-01-04 13:33:09 -0500

    Include command tests

---
+++ tests/test_commands.zsh
@@ -0,0 +1,22 @@
+#!/usr/bin/env zsh
+# shellcheck enable=all
+
+source tests/utilities.zsh
+
+# Run setup
+setup
+
+# Run test for yo update
+answer_should_contain "" "update"
+
+# Run test for yo settings
+answer_should_contain "Llama-3.2-1B-Instruct-Q4_K_M.gguf" "settings"
+
+# Run test for yo settings model.task.temperature
+answer_should_contain "0.2" "settings model.task.temperature"
+
+# Run test for yo settings model.task.temperature
+answer_should_contain "New value for model.task.temperature: 0.1" "settings model.task.temperature 0.1"
+
+# Run cleanup
+cleanup
