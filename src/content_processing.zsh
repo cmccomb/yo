f384130dd16fd178f8e27cf6eedfd0f6be2b5185
Chris McComb <ccmcc2012@gmail.com>
2025-01-04 12:58:54 -0500

    Editing to use settings file

--- src/content_processing.zsh
+++ src/content_processing.zsh
@@ -118,16 +118,17 @@ function extract_facts() {
 	)
 
 	start_llama_session \
-		"${TASK_MODEL_REPO_NAME:-"bartowski/Llama-3.2-1B-Instruct-GGUF"}" \
-		"${TASK_MODEL_FILE_NAME:-"Llama-3.2-1B-Instruct-Q4_K_M.gguf"}" \
+		"$(read_setting model.task.repository)" \
+		"$(read_setting model.task.filename)" \
 		"${prompt}" \
 		"task" \
-		"${COMPRESSION_GENERATION_LENGTH:-"128"}" \
+		"$(read_setting mode.compression.generation_length)" \
 		-1 \
-		0.2 || {
-		echo "Error: Failed to extract facts." >&2
-		return 1
-	}
+		"$(read_setting model.task.temperature)" ||
+		{
+			echo "Error: Failed to extract facts." >&2
+			return 1
+		}
 
 	return 0
 }
@@ -154,7 +155,7 @@ function compress_text() {
 	approximate_length=$(estimate_number_of_tokens "${text}")
 
 	# If length of tokenized text is greater than cutoff, do something
-	if [[ "${approximate_length}" -gt "${COMPRESSION_TRIGGER_LENGTH:-"1024"}" && "${summarize}" == true ]]; then
+	if [[ "${approximate_length}" -gt "$(read_setting mode.compression.trigger_length)" && "${summarize}" == true ]]; then
 
 		# Remove spaces if flag is set
 		if [[ "${remove_spaces}" == true ]]; then
@@ -170,7 +171,7 @@ function compress_text() {
 		local compressed="" chunk_length_in_chars number_of_chunks counter=0
 
 		# Compress text
-		chunk_length_in_chars=$(tokens_to_characters "${COMPRESSION_CHUNK_LENGTH:-"4096"}")
+		chunk_length_in_chars=$(tokens_to_characters "$(read_setting mode.compression.chunk_length)")
 
 		# Estimate how the length of text divided by chunk_length_in_chars
 		number_of_chunks=$(printf "%.0f" $(((${#text} / chunk_length_in_chars) + 1)))
