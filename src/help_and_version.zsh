ca8b31f281d9c218da72b6b876b03efdb3d9c1b6
Chris McComb <ccmcc2012@gmail.com>
2025-01-05 20:11:53 -0500

    Update help_and_version.zsh

--- src/help_and_version.zsh
+++ src/help_and_version.zsh
@@ -9,7 +9,7 @@
 readonly YO="✌️"
 
 ### All versioning follows semver as defined at https://semver.org/ ####################################################
-readonly VERSION="0.1.0"
+readonly VERSION="0.1.1"
 
 ### Display version information ########################################################################################
 function show_version() {
