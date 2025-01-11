e87d3432fefd6e58f3fdbc61735714f0a952c8e1
Chris McComb <ccmcc2012@gmail.com>
2025-01-10 23:16:05 -0500

    Reduced testing

--- tests/test_all.zsh
+++ tests/test_all.zsh
@@ -4,4 +4,5 @@
 zsh tests/test_basic_queries.zsh
 zsh tests/test_static_flags.zsh
 zsh tests/test_local_flags.zsh
-zsh tests/test_online_flags.zsh
+#zsh tests/test_online_flags.zsh
+#zsh tests/test_commands.zsh
