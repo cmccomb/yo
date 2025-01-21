1fada4d4603dec683889f365bf93a422f22aad1e
Chris McComb <ccmcc2012@gmail.com>
2025-01-20 20:25:44 -0500

    Roughing out screenshot and text info to avoid conflicts later

--- src/main.sh
+++ src/main.sh
@@ -306,7 +306,9 @@ prompt=$(
 		"${add_usage_info}" \
 		"${add_system_info}" \
 		"${add_directory_info}" \
-		"${add_clipboard_info}"
+		"${add_clipboard_info}" \
+		"${add_screenshot_info}" \
+		"${add_text_info}"
 ) || {
 	echo "Error: Failed to generate prompt." >&2
 	exit 1
