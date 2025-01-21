a268bf5985520012e45ddc698de9ab306eb0bf95
Chris McComb <ccmcc2012@gmail.com>
2025-01-20 20:24:54 -0500

    Roughing out screenshot and text info to avoid conflicts later

--- src/main.sh
+++ src/main.sh
@@ -207,6 +207,12 @@ while [ $# -gt 0 ]; do
 		surf_and_add_results=true
 		;;
 
+  # Add screenshot info
+  -sc|--screenshot) add_screenshot_info=true ;;
+
+  # Add text info
+  -t|--text) add_text_info=true ;;
+
 	# Add system information to the context
 	-y | --system) add_system_info=true ;;
 
