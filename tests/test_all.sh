e7ddaa70100960dc56b7a98b71d157990572302a
Chris McComb <ccmcc2012@gmail.com>
2025-08-22 16:37:45 -0400

    Bringing the branch up to date (#51)
    * Fixing emoji issue. Why check this in the first place?
    
    * Updated models
    
    * Updated models
    
    * Fixed model names
    
    * test: add help and version tests (#46)
    
    * Fix TIFF pattern in content processing (#49)
    
    * docs: fix URL comment (#47)
    
    * docs: clarify token conversion comments (#48)
    
    * Strip reasoning tags from Qwen3 responses (#45)
    
    * Test removal of reasoning tags
    
    * Use Qwen3 1.7B task model
    
    * Update settings.sh
    
    Undoing indent issues
    
    * Update settings.sh
    
    * V0.2.2 updates (#50)
    
    * Bumping version number
    
    * Added help text for --text
    
    * Added help text for --screenshot
    
    * Roughing out screenshot and text info to avoid conflicts later
    
    * Roughing out screenshot and text info to avoid conflicts later
    
    * Roughing out screenshot and text info to avoid conflicts later
    
    * Roughing out screenshot and text info to avoid conflicts later
    
    * Roughing out screenshot and text info to avoid conflicts later
    
    * Roughing out screenshot and text info to avoid conflicts later
    
    * Roughing out screenshot and text info to avoid conflicts later
    
    * Text flag is working!
    
    * Formatting
    
    * Initial implementation, but its hanging somewhere
    
    * Documentation
    
    * Documentation

--- tests/test_all.sh
+++ tests/test_all.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env sh
 # shellcheck enable=all
 
+sh tests/test_reasoning_tags.sh
 sh tests/test_basic_queries.sh
 sh tests/test_static_flags.sh
 sh tests/test_local_flags.sh
 sh tests/test_online_flags.sh
+sh tests/test_help_and_version.sh
 #sh tests/test_commands.sh
